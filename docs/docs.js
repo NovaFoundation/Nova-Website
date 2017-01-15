ece740865bd79a9acda5221f2e056421c8d9be35
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 06:33:48 -0800

    Updated to load sub-states.

--- docs/docs.js
+++ docs/docs.js
@@ -1,16 +1,29 @@
-angular.module("nova").controller("DocsController", ["$scope", function ($scope) {
-    $scope.pages = [{
-        header: "nothign",
-        subheader: "if defsird",
-        template: "",
-        children: []
-    }];
-    
-    $scope.page = $scope.pages[0];
-    
+window.docsPages = [{
+    header: "Class Types",
+    url: "class-types",
+    children: []
+}, {
+    header: "HELLO WORLD",
+    url: "hello-world",
+    children: []
+}];
+
+angular.module("nova").controller("DocsController", ["$scope", "$rootScope", function ($scope, $rootScope) {
     $scope.sharePage = function (page) {
         page = page || $scope.page;
         
         alert("sharing " + page.header);
     };
+    
+    function stateUpdated() {
+        var url = $rootScope.state.url.substring(1);
+        
+        $scope.page = window.docsPages.find(function (d) {
+            return d.url == url;
+        });
+    }
+    
+    stateUpdated();
+    
+    $rootScope.$on("stateUpdated", stateUpdated);
 }]);
