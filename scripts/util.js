e5ec13e4baa4a56ee8f917fa606434e812e3251d
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 03:12:08 -0800

    Added query params parsing/encoding code.

--- scripts/util.js
+++ scripts/util.js
@@ -184,3 +184,39 @@
         flashVersion: flashVersion
     };
 }(this));
+
+function getQueryParams() {
+    var queryParams = {};
+    var e,
+        a = /\+/g,  // Regex for replacing addition symbol with a space
+        r = /([^&;=]+)=?([^&;]*)/g,
+        d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+        q = window.location.search.substring(1);
+
+    while (e = r.exec(q))
+       queryParams[d(e[1])] = d(e[2]);
+
+    return queryParams;
+}
+
+function getQueryString(params) {
+    var url = "";
+    
+    var index = 0;
+    
+    Object.keys(params).forEach(function (key) {
+        var value = params[key];
+        
+        if (value) {
+            if (index++ > 0) {
+                url += "&";
+            }
+            
+            url += encodeURIComponent(key) + "=" + encodeURIComponent(value);
+        }
+    });
+    
+    return url;
+}
+
+var queryParams = getQueryParams();
