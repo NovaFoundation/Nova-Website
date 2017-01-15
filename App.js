094a71540d9b1e43716f70a498568b464522e644
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 03:45:56 -0800

    Added capitalize filter.

--- App.js
+++ App.js
@@ -1,1 +1,5 @@
-var app = angular.module("nova", ['ngSlide']);
+var app = angular.module("nova", ['ngSlide']);var app = angular.module("nova", ['ngSlide', 'ui.router']).filter('capitalize', function() {
+    return function(input) {
+      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+    }
+});
