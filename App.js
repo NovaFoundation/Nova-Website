1ad2b7dd73a85a420e41b4ef78e3647e8941b6a5
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 03:46:08 -0800

    Added ui-router reference.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-var app = angular.module("nova", ['ngSlide']);var app = angular.module("nova", ['ngSlide', 'ui.router']).filter('capitalize', function() {
+var app = angular.module("nova", ['ngSlide', 'ui.router']).filter('capitalize', function() {
     return function(input) {
       return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
     }
