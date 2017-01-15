9ea1cec1f6ce78814f08b6ca21b37f1151c87815
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 03:38:29 -0800

    Added os code.

--- DownloadController.js
+++ DownloadController.js
@@ -1,3 +1,13 @@
 angular.module("nova").controller("DownloadController", ["$scope", function ($scope) {
+    $scope.os = jscd.os;
+    $scope.osVersion = jscd.osVersion;
+    $scope.lowerOs = $scope.os.toLowerCase();
+    
+    var validOses = ["windows", "mac", "linux"];
+    
+    // clean here
+    
+    $scope.stableOses = [$scope.lowerOs];
+    $scope.betaOses = [$scope.lowerOs];
     
 }]);
