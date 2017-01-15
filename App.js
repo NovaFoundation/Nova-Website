6cf243c407dbce1d6b937f423d0192d8f910b67c
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 06:32:15 -0800

    Added uiRouterStyles injection.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-var app = angular.module("nova", ['ngSlide', 'ui.router']).filter('capitalize', function() {
+var app = angular.module("nova", ['ngSlide', 'ui.router', 'uiRouterStyles']).filter('capitalize', function() {
     return function(input) {
       return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
     }
