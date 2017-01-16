1af94dd7f07181521d4484044418592e8fbd3016
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 18:07:15 -0800

    Added $safeApply function.

--- master.js
+++ master.js
@@ -1,3 +1,15 @@
-angular.module("nova").controller("MasterController", ["$scope", function ($scope) {
+angular.module("nova").controller("MasterController", ["$scope", "$rootScope", function ($scope, $rootScope) {
     $scope.jump = jump;
+    
+    $rootScope.$safeApply = function (fn) {
+         var phase = $scope.$root.$$phase;
+         
+         if (phase == '$apply' || phase == '$digest') {
+             if (fn && typeof fn === 'function') {
+                 fn();
+             }
+         } else {
+             $scope.$apply(fn);
+         }
+     };
 }]);
