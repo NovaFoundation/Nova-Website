2fb24371c01dfb833b27bb7df72aa99d9a71e66a
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 18:09:32 -0800

    Removed old url.

--- scripts/routeConfig.js
+++ scripts/routeConfig.js
@@ -33,7 +33,6 @@ angular.module("nova").config(['$stateProvider', '$urlRouterProvider', '$locatio
     });
     
     $stateProvider.state('download', {
-        url: '/download',
         url: '/download?version',
         templateUrl: '/download.html',
         controller: 'DownloadController',
