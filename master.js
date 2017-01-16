de374e5177e00239fa9e4c3663068b8d203203a1
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-15 18:07:26 -0800

    Added addEventListener function.

--- master.js
+++ master.js
@@ -1,6 +1,24 @@
 angular.module("nova").controller("MasterController", ["$scope", "$rootScope", function ($scope, $rootScope) {
     $scope.jump = jump;
     
+    var listeners = {};
+    
+    $rootScope.addEventListener = function (type, callback, fireOnAdd) {
+        listeners[type] = listeners[type] || [];
+        
+        listeners[type].push(callback);
+        
+        if (fireOnAdd) {
+            callback();
+        }
+        
+        $rootScope.$on(type, function () {
+            callback.apply($rootScope, [].slice.call(arguments));
+        });
+        
+        return callback;
+    };
+    
     $rootScope.$safeApply = function (fn) {
          var phase = $scope.$root.$$phase;
          
