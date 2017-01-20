28190e9b043c3443bbea993816acbede4321541b
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-20 05:54:21 -0800

    Added oses.

--- scripts/services/os.js
+++ scripts/services/os.js
@@ -3,9 +3,25 @@ angular.module("nova").service("$os", [function () {
     this.osVersion = jscd.osVersion;
     this.lowerOs = this.os.toLowerCase();
     
-    if (this.lowerOs == "windows") {
-        this.osExecutableExtension = ".exe";
-    } else {
-        this.osExecutableExtension = "";
     }
+    
+    this.oses = {
+        windows: {
+            name: "Windows",
+            key: "windows",
+            extension: ".exe"
+        }, 
+        mac: {
+            name: "Mac OS X",
+            key: "mac",
+            extension: ""
+        }, 
+        linux: {
+            name: "Linux",
+            key: "linux",
+            extension: ""
+        }
+    };
+    
+    this.currentOs = this.oses[this.lowerOs];
 }]);
