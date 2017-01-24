88f1703606993981f649199094081e941ce5af52
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-24 15:53:01 -0800

    Added first/last array functions.

--- scripts/util.js
+++ scripts/util.js
@@ -344,3 +344,15 @@ window.addEventListener("load", function () {
         }
     }
 } )( jQuery );
+
+if (!Array.prototype.first){
+    Array.prototype.first = function(){
+        return this[0];
+    };
+};
+
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
+};
