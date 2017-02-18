03d7bb07b81f6dcd9a9ade3a9c6d6a172ea586d0
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-18 13:11:09 -0800

    Added blog controller

---
+++ blog/blog.js
@@ -0,0 +1,59 @@
+window.blogPages = [{
+    header: "Nova Compiler Design",
+    url: "compiler-design",
+    date: "2/18/2017",
+    author: "Braden Steffaniak"
+}];
+
+angular.module("nova").controller("BlogController", ["$scope", "$rootScope", function ($scope, $rootScope) {
+    $scope.sharePage = function (page) {
+        page = page || $scope.page;
+        
+        alert("sharing " + page.header);
+    };
+    
+    $scope.pages = blogPages;
+    
+    function recursiveApply(func) {
+        function action(page) {
+            func(page);
+            
+            if (page.children) {
+                page.children.forEach(action);
+            }
+        }
+        
+        $scope.pages.forEach(action);
+    }
+    
+    recursiveApply(function (page) {
+        page.selected = false;
+        page.open = false;
+    });
+    
+    function stateUpdated() {
+        var url = $rootScope.state.url.substring(1);
+        
+        $scope.page = $rootScope.state.state.data.page;
+        
+        var current = $scope.page;
+        
+        if (current) {
+            recursiveApply(function (page) {
+                page.selected = false;
+            });
+    
+            current.selected = true;
+            
+            while (current) {
+                current.open = true;
+                
+                current = current.parent;
+            }
+        }
+    }
+    
+    stateUpdated();
+    
+    $rootScope.$on("stateUpdated", stateUpdated);
+}]);
