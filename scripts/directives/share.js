d0bc1d686824e368fc4166de39f07a753fc2e80f
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-18 23:44:41 -0800

    Added fbShare function

--- scripts/directives/share.js
+++ scripts/directives/share.js
@@ -30,6 +30,16 @@ angular.module("nova").directive("share", ['$templateCache', '$compile', functio
                 
                 document.addEventListener("click", close);
             });
+            
+            scope.fbShare = function(url, title, descr, winWidth, winHeight) {
+                var winTop = (screen.height / 2) - (winHeight / 2);
+                var winLeft = (screen.width / 2) - (winWidth / 2);
+                
+                title = title || "";
+                descr = descr || "";
+                
+                window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+            };
         }
     }
 }]);
