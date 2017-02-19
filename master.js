1e2f175ae0da5bba3228382d7fb5ada544570d54
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-18 23:45:06 -0800

    Fixed indentation

--- master.js
+++ master.js
@@ -28,14 +28,14 @@ angular.module("nova").controller("MasterController", ["$scope", "$rootScope", "
     };
     
     $rootScope.$safeApply = function (fn) {
-         var phase = $scope.$root.$$phase;
-         
-         if (phase == '$apply' || phase == '$digest') {
-             if (fn && typeof fn === 'function') {
-                 fn();
-             }
-         } else {
-             $scope.$apply(fn);
-         }
-     };
+        var phase = $scope.$root.$$phase;
+
+        if (phase == '$apply' || phase == '$digest') {
+            if (fn && typeof fn === 'function') {
+                fn();
+            }
+        } else {
+            $scope.$apply(fn);
+        }
+    };
 }]);
